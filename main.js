3f0219ffeef81eb797b7a477744986636faa7c4e
Deividas Rimkus <8516648+Lisviks@users.noreply.github.com>
2021-03-28 14:12:48 +0100

    Notifications

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Notification } = require('electron');
 const path = require('path');
+
+function showNotification() {
+  const notification = {
+    title: 'Basic Notification',
+    body: 'Notification from the Main process',
+  };
+  new Notification(notification).show();
+}
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -13,15 +21,18 @@ function createWindow() {
   win.loadFile('index.html');
 }
 
-app.whenReady().then(() => {
-  createWindow();
+app
+  .whenReady()
+  .then(() => {
+    createWindow();
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
-});
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+      }
+    });
+  })
+  .then(showNotification);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
